f024f3b9231e55a943ded42f6768ebbd080410cf
Andy Aziri <andy.aziri@gmail.com>
2022-03-10 19:37:18 +0100

    added function which moves cursor to specified coordinates which are given through 'x' and 'y' variables

---
+++ WebSocket/MouseControl/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MouseControl
+{
+    public class Class1
+    {
+        public void MoveCursor(int x, int y)
+        {
+            Form1 cursor = new Form1();
+
+            while (true)
+            {
+                cursor.Move(x, y);
+                x++;
+                y++;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ WebSocket/MouseControl/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseControl
+{
+    public class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        public void Move(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+    }
+}
